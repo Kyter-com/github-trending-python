3700875da73ef651c18419f333c2e1d4098123c8
Nick Reisenauer <nickdreisenauer@gmail.com>
2023-08-06 19:45:30 -0700

    feat: successfully posting url

--- main.py
+++ main.py
@@ -11,7 +11,18 @@ url = requests.get(
     headers={"Authorization": f"Bearer {os.getenv('CLOUDFLARE_KV_KEY')}"},
     timeout=10,
 )
-
 url = url.content.decode("utf-8")
 
-print(url)
+mastodon_post = requests.post(
+    "https://botsin.space/api/v1/statuses",
+    headers={
+        "Authorization": f"Bearer {os.getenv('MASTODON_ACCESS_TOKEN')}",
+        "Content-Type": "application/x-www-form-urlencoded",
+    },
+    timeout=30,
+    data={"status": f"{url}"},
+)
+mastodon_post.raise_for_status()
+
+
+print("✅ Successfully posted to Mastodon")
