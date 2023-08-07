9aabdd50922258f0816dadf860bbd2ae4f918c14
Nick Reisenauer <nickdreisenauer@gmail.com>
2023-08-06 18:58:22 -0700

    feat: grab value out of kv

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
 print("🏁 Starting Bot")
+
+url = requests.get(
+    f"{os.getenv('CLOUDFLARE_KV_URL')}/python",
+    headers={"Authorization": f"Bearer {os.getenv('CLOUDFLARE_KV_KEY')}"},
+    timeout=10,
+)
+
+url = url.content.decode("utf-8")
+
+print(url)
