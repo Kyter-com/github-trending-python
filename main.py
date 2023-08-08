588eef2cf515b9c3ababf9ad0445d87bcea59f83
Nick Reisenauer <nickdreisenauer@gmail.com>
2023-08-07 17:44:02 -0700

    feat: update post with stars

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ load_dotenv()
 print("🏁 Starting Bot")
 
 url = requests.get(
-    f"{os.getenv('CLOUDFLARE_KV_URL')}/python",
+    f"{os.getenv('CLOUDFLARE_KV_URL')}/values/python",
     headers={"Authorization": f"Bearer {os.getenv('CLOUDFLARE_KV_KEY')}"},
     timeout=10,
 )
 url = url.content.decode("utf-8")
+
+metadata = requests.get(
+    f"{os.getenv('CLOUDFLARE_KV_URL')}/metadata/python",
+    headers={"Authorization": f"Bearer {os.getenv('CLOUDFLARE_KV_KEY')}"},
+    timeout=10,
+)
+metadata = metadata.json()
 
 mastodon_post = requests.post(
     "https://botsin.space/api/v1/statuses",
@@ -20,7 +27,12 @@ mastodon_post = requests.post(
         "Content-Type": "application/x-www-form-urlencoded",
     },
     timeout=30,
-    data={"status": f"{url}"},
+    data={
+        "status": f"{metadata['result']['author_name']}/"
+        + f"{metadata['result']['repo_name']}"
+        + f" - ⭐ {metadata['result']['total_stars']}"
+        + f"\n{url}"
+    },
 )
 mastodon_post.raise_for_status()
 
